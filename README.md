9af0254b362ba0d4fe09c153cf7eb1ad0709b742
Nathan Tjahjadi <nathantja@yahoo.com>
2023-10-10 16:30:05 -0500

    starter

---
+++ README.md
@@ -0,0 +1,8 @@
+## R33 Choose Your Own Adventure Sprint
+
+Wishlist:
+- write more tests
+- map features
+
+
+- presign images in the future, currently accessible if you have any filename
---
+++ test-scripts.py
@@ -0,0 +1,17 @@
+import boto3
+
+BUCKET_NAME = "r33anotherbnb"
+
+s3 = boto3.resource('s3')
+
+
+# s3_client = boto3.client(
+#     's3',
+#     aws_access_key_id = ,
+#     aws_secret_access_key
+# )
+
+# Upload a new file
+with open('test.jpg', 'rb') as data:
+    s3.Bucket(f"{BUCKET_NAME}").put_object(Key='test.jpg', Body=data)
+
